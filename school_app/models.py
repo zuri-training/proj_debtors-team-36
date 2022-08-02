d667650318b6d57bdfcf6df385310449671327d4
ritasylvia <impeccablesylvia@gmail.com>
2022-08-02 21:48:02 +0100

    Debtors models

--- school_app/models.py
+++ school_app/models.py
@@ -1,3 +1,20 @@
+from unicodedata import name
 from django.db import models
+from django.contrib.auth import get_user_model
+from django.utils import timezone
 
-# Create your models here.
+
+class Debtors(models.Model):
+    GENDER_CHOICES = ("Male", "Female", "Other")
+    id = models.ForeignKey(primary_key=True, blank=False)
+    name = models.CharField(max_length=50)
+    gender = models.CharField(choices=GENDER_CHOICES.choices, max_length=10, null=False)
+    amount_owed = models.IntegerField(default=0)
+    amount_cleared = models.IntegerField(default=0)
+
+    class Meta:
+        ordering = ['name']
+
+        
+
+ 
