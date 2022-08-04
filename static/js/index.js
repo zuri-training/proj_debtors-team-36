44d52e0ab5c8ff044b77d2c4ee255faf7f59b159
Iyin0 <p2iyinoluwa@gmail.com>
2022-08-04 19:06:17 +0100

    hi-fi update of topNav, footer, sideNav and search

--- static/js/index.js
+++ static/js/index.js
@@ -26,14 +26,6 @@ const cookies_list = [
     }
 ]
 
-const currentPage = window.location.pathname
-const navLinks = document.querySelectorAll('nav a').
-    forEach(link => {
-        if (link.href.includes(`${currentPage}`)) {
-            link.classList.add('active');
-        }
-    })
-
 const removePopup = () => {
     const remove = document.querySelector('.cookies-back')
     return remove.remove()
@@ -126,75 +118,4 @@ const showCookies = () => {
     document.body.appendChild(cookies_back)
 }
 
-const callCookies = setTimeout(showCookies, 3000);
-
-// /* The container */
-// .checkboxes {
-//     display: block;
-//     position: relative;
-//     padding-left: 35px;
-//     /* margin-bottom: 12px; */
-//     margin: 0px 0px 27px 0px;
-//     cursor: pointer;
-//     font-size: 20px;
-//     font-weight: 400;
-    // -webkit-user-select: none;
-    // -moz-user-select: none;
-    // -ms-user-select: none;
-//     user-select: none;
-// }
-
-// /* Hide the browser's default checkbox */
-// .checkbox {
-//     position: absolute;
-//     opacity: 0;
-//     cursor: pointer;
-//     height: 0;
-//     width: 0;
-// }
-
-// /* Create a custom checkbox */
-// .checkmark {
-//     position: absolute;
-//     top: 0;
-//     left: 0;
-//     height: 25px;
-//     width: 25px;
-//     background-color: #fcfcfc;
-//     border: 1.5px solid #000033;
-// }
-
-// /* On mouse-over, add a grey background color */
-// .checkboxes:hover input~.checkmark {
-//     background-color: #ccc;
-// }
-
-// /* When the checkbox is checked, add a blue background */
-// .checkboxes input:checked~.checkmark {
-//     background-color: #2196F3;
-// }
-
-// /* Create the checkmark/indicator (hidden when not checked) */
-// .checkmark:after {
-//     content: "";
-//     position: absolute;
-//     display: none;
-// }
-
-// /* Show the checkmark when checked */
-// .checkboxes input:checked~.checkmark:after {
-//     display: block;
-// }
-
-// /* Style the checkmark/indicator */
-// .checkboxes .checkmark:after {
-//     left: 9px;
-//     top: 5px;
-//     width: 5px;
-//     height: 10px;
-//     border: solid white;
-//     border-width: 0 3px 3px 0;
-//     -webkit-transform: rotate(45deg);
-//     -ms-transform: rotate(45deg);
-//     transform: rotate(45deg);
-// }
+// const callCookies = setTimeout(showCookies, 3000);  // to call the cookies
