a716bb11ecf6e19a4fe88dc6d6c3da5cc8c1941c
Hannah Opara <oparanaomi123@gmail.com>
2022-08-05 17:55:37 +0100

    Javascript for popups

---
+++ static/js/add-new-debtor.js
@@ -0,0 +1,10 @@
+
+        let popup = document.getElementById("popup");
+
+        function openPopup() {
+            popup.classList.add("open-popup");
+        }
+
+        function closePopup() {
+            popup.classList.remove("open-popup");
+        }
