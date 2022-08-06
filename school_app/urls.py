509ce37012741ade5244c21f7d69a8895bb00136
Johnchizzy <johnchizzy85@gmail.com>
2022-08-06 02:22:59 +0100

    update settings

--- school_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = "project_app"
-
-urlpatterns = [
-    path("contend/", views.contend, name="contend_debt"),
-    path("contact/", views.contend, name="contend_us"),
-]
--- static/js/top-nav.js
+++
@@ -1,7 +0,0 @@
-const currentPage = window.location.pathname
-const navLinks = document.querySelectorAll('nav a').
-    forEach(link => {
-        if (link.href.includes(`${currentPage}`)) {
-            link.classList.add('active');
-        }
-    })
