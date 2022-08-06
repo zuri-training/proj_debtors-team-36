d2c9f1553a4682ccb54d269e02c45433d0c1890c
udeani <udeani.henry@gmail.com>
2022-08-06 14:12:55 +0100

    In other to replicate or get the server running, these edits where made:
    Newly installed apps:
            pip install uuid
    
    Places Edit were made:
    
    1. modified the way secret key was set: setting up a default secret key for anyone that wants to test the code only without having to define their own key.
    
    2. adding AUTH_USER_MODEL = 'school_app.School' to project settings.py file to avoid error E304 conflicts
    
    3. changed INSTALLED_APPS list to 'school_app','uuid','django_extensions' to avoid app not found error
    
    4. installed uuid
            imported uuid to models.py
            changed the Debtors class id from "models.ForeignKey(primary_key=True, blank=False, on_delete=models.CASCADE)" to models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False) to escape foreign key error of 'to ( where it points to)' and 'on_delete' error.
            Note: uuid creates a long string of id which can be sliced. It was used without truncation just to solely avoid the above mentioned error.
    
    5. Changed the GENDER_CHOICES of Debtors class from a tuple to django text choice class to avoid calling a mere attribute with choice function which it does not possess.

--- school_prj/settings.py
+++ school_prj/settings.py
@@ -61,7 +61,7 @@ ROOT_URLCONF = 'school_prj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'static'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR/'static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
