30b29ffe3d6f918aae8ff605f6549dfc0055d31f
tamazing <taiwolasaki@gmail.com>
2022-08-07 16:45:04 +0100

    worked on index, signup, login, contact_us, views and urls

--- school_app/views.py
+++ school_app/views.py
@@ -1,3 +1,99 @@
-from django.shortcuts import render
+#from django.http import HttpResponse
+from django.shortcuts import redirect, render
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, logout, authenticate
+from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from .forms import School_RegForm, RoomForm, UserForm
+from .models import Debtors, School, Debtor_list, School_Post
+
+
 
 # Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def contact_view(request):
+
+    return render(request, 'contact-us.html', {})
+
+
+def about_view(request):
+    return render(request, 'about-us.html')
+
+# @login_required(login_url = '/login/')
+
+
+def dashboard(request):
+    user = request.user.username
+    return render(request, 'dashboard.html', {'username': user})
+
+
+def signup_view(request):
+    # if request.user.is_authentecated:
+    #     return redirect('home')
+    if request.method == 'POST':
+        form = School_RegForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            # email = form.cleaned_data.get('email')
+            # password = form.cleaned_data.get('password1')
+            # user = authenticate(email=email, password=password)
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            form = School_RegForm(request.POST)
+            return render(request, 'signup.html', {'form': form})
+    else:
+        form = School_RegForm(request.POST)
+    return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    # if request.user.is_authentecated:
+    # return redirect('dashboard')
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('dashboard')
+        else:
+            form = AuthenticationForm()
+            return render(request, 'login.html', {'form': form})
+    else:
+        form = AuthenticationForm()
+        return render(request, 'login.html', {'form': form})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('signin')
+
+def current_debtors(request):
+    debtors = Debtors.objects.all().order_by('name')
+    return render(request, 'current-debtors.html', {'debtors': debtors})
+
+
+# @login_required(login_url = '/login/')
+#def add_debtor(request):
+    if request.method == 'POST':
+        form = CreateView(request.POST, request.FILES)
+        if form.is_valid():
+            # save to database
+            debtor = form.save(commit=FALSE)
+            debtor.author = request.user
+            debtor.save()
+            return redirect('debt-summary')
+    else:
+        form = CreateView(request.POST, request.FILES)
+    return render(request, 'add-new-debtor.html')
+
+# @login_required(login_url = '/login/')
+# @login_required(login_url = '/login/')
+# def debtor_detail(request):
+    debtors = Debtors.objects.get(id)
+    return render(request, 'debtor-detail.html')
+
