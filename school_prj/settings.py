338b350ad644390f1a46e0abfe4c1b72eaf5d7ea
Adelakin Israel <adelakinisrael024@gmail.com>
2022-08-14 22:37:39 +0100

    whitenoise updated

--- school_prj/settings.py
+++ school_prj/settings.py
@@ -58,6 +58,9 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
+
 ]
 
 ROOT_URLCONF = 'school_prj.urls'
